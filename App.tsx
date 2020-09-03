9e995061af25736dc3ce4bc23b82758067723550
anuragtiwari1 <anuragkumartiwari79@gmail.com>
2020-09-03 21:04:38 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import * as React from 'react';
+import AppRouter from './Router';
+
+export default function App() {
+  return <AppRouter />;
+}
---
+++ Router.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './pages/landing/landing.controller';
+
+const Stack = createStackNavigator();
+
+const AppRoutes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="landing" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppRoutes;
